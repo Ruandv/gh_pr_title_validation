aa38f04ff56997e3f333226a90e9d4b76ebb94bb
Ruan De Villiers <ruan.devilliers@nintex.com>
2024-05-10 17:07:43 +0200

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,61 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.run = void 0;
+const core = __importStar(require("@actions/core"));
+const github = __importStar(require("@actions/github"));
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // The `who-to-greet` input is defined in action metadata file
+            const whoToGreet = core.getInput('who-to-greet', { required: true });
+            core.info(`Hello, ${whoToGreet}!`);
+            // Get the current time and set as an output
+            const time = new Date().toTimeString();
+            core.setOutput('time', time);
+            // Output the payload for debugging
+            core.info(`The event payload: ${JSON.stringify(github.context.payload, null, 2)}`);
+        }
+        catch (error) {
+            // Fail the workflow step if an error occurs
+            core.setFailed(error.message);
+        }
+    });
+}
+exports.run = run;
+//# sourceMappingURL=main.js.map
